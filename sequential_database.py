5aadde38d7dc8338073880ee5cce3bc0474050f8
Cruz-Leung <pleung@student.saintaug.nsw.edu.au>
2025-06-22 00:36:00 +1000

    added database

---
+++ sequential_database.py
@@ -0,0 +1,8 @@
+sequential_patterns = [
+        # Numbers
+        '012', '123', '234', '345', '456', '567', '678', '789', '890',
+        # Letters
+        'abc', 'bcd', 'cde', 'def', 'efg', 'fgh', 'ghi', 'hij', 'ijk', 'jkl',
+        'klm', 'lmn', 'mno', 'nop', 'opq', 'pqr', 'qrs', 'rst', 'stu',
+        'tuv', 'uvw', 'vwx', 'wxy', 'xyz'
+    ]
